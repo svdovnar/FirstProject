a2ddb11359ae86d6a7ea284885711f420bf49f16
ashoshosho <svdovnar3@gmail.com>
2020-05-12 21:25:14 +0300

    initial commit

---
+++ lection1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lection1
+{
+    internal class Program
+    {
+        private int _age;
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("First app");
+        }
+    }
+}
