cd94e319c24eccf56f39c074e373d691917e9d43
ashoshosho <svdovnar3@gmail.com>
2020-05-19 19:48:29 +0300

    Food simulator

--- lection1/Program.cs
+++ lection1/Program.cs
@@ -4,9 +4,9 @@ namespace lection1
 {
     internal class Program
     {
-        private int _age;
         public static void Main(string[] args)
         {
+            Console.WriteLine("First app");
             Console.WriteLine("First app");
         }
     }
