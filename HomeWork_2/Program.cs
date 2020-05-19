ec8c2ea0164fa8caf186a5aa2e5b68a4d4c31175
ivan_khomichkov <zloivan@mail.ru>
2020-05-19 22:17:07 +0300

    all code

---
+++ HomeWork_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork_2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+
+    public static class DisciplinesGenerator
+    {
+        private static string[] all = {"Mathematica","Chemistry","Biolagy", "Phisics"};
+        public static string GetRandomDiscipline()
+        {
+            Random rand = new Random();
+            var randIndex = rand.Next(0, all.Length);
+            return all[randIndex];
+        }
+    }
+
+    class Student
+    {
+        public string Name;
+
+        public List<string> Disciplines  = new List<string>();
+
+
+
+        public Dictionary<string, float> markByDiscipline = new Dictionary<string, float>();
+
+        public void DoSomething()
+        {
+           var randomDiscipline = DisciplinesGenerator.GetRandomDiscipline();
+        }
+    }
+}
---
+++ HomeWork_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HomeWork_2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HomeWork_2")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("19259C4D-3B28-4B38-9C5E-9866AF3B5757")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
