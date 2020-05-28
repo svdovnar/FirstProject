5adb4a7facdde1a8be483365f076e15f9d5d857d
ivan_khomichkov <zloivan@mail.ru>
2020-05-28 22:43:28 +0300

    Class basic

---
+++ ClassBasic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassBasic
+{
+    internal class Program
+    {
+        private int progrumNumber;
+        public static void Main(string[] args)
+        {
+            Student vasya;
+
+            
+            var students = Student.GetAllStudents();
+            vasya = new Student("Vasya");
+
+            vasya.Age = 18;
+            //vasya = "Vsya";
+
+            vasya.UpdateCourses(new Course("Botanica"));
+            var listOfVasyasCources = vasya.MarksByCourses;
+            //vasya.MarksByCourses = new List<Course>();
+            //vasya.allSutdentCourcses.Clear();
+
+
+            float avarageMarkOfVasya = vasya.ClculateAvarage();
+
+        }
+    }
+}
---
+++ ClassBasic/Student.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.Net.Mime;
+
+namespace ClassBasic
+{
+    public  class Student
+    {
+        public static string Name;
+
+        protected string LastName;
+        public int Age;
+        public int CourseNumber;
+        private List<Course> allSutdentCourcses = new List<Course>();
+        public Dictionary<Course, int> marksByCourses;
+        public float AvarageMark;
+        private float result;
+
+        private static List<Student> students = new List<Student>();
+
+        public  Student(string name)
+        {
+            Name = name;
+
+            students.Add(this);
+        }
+
+
+
+
+        public float ClculateAvarage()
+        {
+             float devideResult = result / marksByCourses.Count;
+
+            return devideResult;
+        }
+
+
+
+        private void OutputData(int i)
+        {
+            Age += 5;
+        }
+
+        private int ClaculateAvarage(object data)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private object GetDataFromServer()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DismissStudent()
+        {
+            ///
+        }
+
+        public void UpdateCourses(Course botanic)
+        {
+            allSutdentCourcses.Add(botanic);
+        }
+
+        // private List<Course> courses;
+        //
+        // public List<Course> GetCourses()
+        // {
+        //     return courses;
+        // }
+        //
+        // public void SetCourses(List<Course> value)
+        // {
+        //     courses = value;
+        // }
+
+        public List<Course> MarksByCourses { get; private set; }
+
+        public List<Course> GetCourses()
+        {
+            MarksByCourses.Add(new Course("dqcwd"));
+            return allSutdentCourcses;
+        }
+
+        public static List<Student> GetAllStudents()
+        {
+            return students;
+        }
+    }
+
+    public class Course
+    {
+        public Course(string botanica)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
