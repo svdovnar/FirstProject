434033526df8a18b223fad908ac89e7a42560a9e
ashoshosho <svdovnar3@gmail.com>
2020-06-03 20:34:30 +0300

    Triangle changes

--- TrinagleHomeWork/Program.cs
+++ TrinagleHomeWork/Program.cs
@@ -5,10 +5,20 @@ namespace TrinagleHomeWork
     internal class Program
     {
         public static void Main(string[] args)
-        {
-            Triangle triangle = new Triangle();
-            Console.WriteLine();
-            Triangle triangleNext = new Triangle();
+        { 
+           Triangle triangle = new Triangle();
+           var sideAB = triangle.GetSideAB();
+           Console.WriteLine(sideAB);
+           var sideBC = triangle.GetSideBC();
+           Console.WriteLine(sideBC);
+           var sideCA = triangle.GetSideCA();
+           Console.WriteLine(sideCA);
+           var perimeter = triangle.GetPerimeter();
+           Console.WriteLine(perimeter);
+           var square = triangle.GetSquare();
+           Console.WriteLine(square);
+           var angleA = triangle.GetAngleA();
+           Console.WriteLine(angleA);
         }
     }
 }
--- TrinagleHomeWork/Triangle.cs
+++ TrinagleHomeWork/Triangle.cs
@@ -22,17 +22,7 @@ namespace TrinagleHomeWork
             y2 = 4;
             x3 = 3;
             y3 = 2;
-            AB = Math.Sqrt(Math.Pow((y1 - x1), 2) + Math.Pow((y2 - x2), 2));
-            BC = Math.Sqrt(Math.Pow((x3 - x2), 2) + Math.Pow((y3 - y2), 2));
-            CA = Math.Sqrt(Math.Pow((x3 - x1), 2) + Math.Pow((y3 - y1), 2));
-            AngleA = Math.Acos((AB * AB + CA * CA - BC * BC) / (2 * AB * CA)) * 180 / Math.PI;
-            AngleB = Math.Acos((AB * AB + BC * BC - CA * CA) / (2 * AB * BC)) * 180 / Math.PI;
-            AngleC = Math.Acos((BC * BC + CA * CA - AB * AB) / (2 * BC * CA)) * 180 / Math.PI;
-            Perimeter = AB + BC + CA;
-            p = Perimeter/2;
-            Square = Math.Sqrt(p*(p-AB)*(p-BC)*(p-CA));
-            Print();
-
+            
         }
         public Triangle (double x1, double y1, double x2, double y2, double x3, double y3)
         {
@@ -42,31 +32,58 @@ namespace TrinagleHomeWork
             this.y2 = y2;
             this.x3 = x3;
             this.y3 = y3;
+            
+        }
+
+        public double GetSideAB()
+        {
+            AB = Math.Sqrt(Math.Pow((y1 - x1), 2) + Math.Pow((y2 - x2), 2));
+            return AB;
+        }
+        public double GetSideBC()
+        {
+            BC = Math.Sqrt(Math.Pow((x3 - x2), 2) + Math.Pow((y3 - y2), 2));
+            return BC;
+        }
+        public double GetSideCA()
+        {
+            CA = Math.Sqrt(Math.Pow((x3 - x1), 2) + Math.Pow((y3 - y1), 2)); 
+            return CA;
+        }
+
+        public double GetAngleA()
+        {
+            AngleA = Math.Acos((AB * AB + CA * CA - BC * BC) / (2 * AB * CA)) * 180 / Math.PI;
+            return AngleA;
+        }
+
+        public double GetAngleB()
+        {
+            AngleB = Math.Acos((AB * AB + BC * BC - CA * CA) / (2 * AB * BC)) * 180 / Math.PI;
+            return AngleB;
+        }
+
+        public double GetAngleC()
+        {
+            AngleC = Math.Acos((BC * BC + CA * CA - AB * AB) / (2 * BC * CA)) * 180 / Math.PI;
+            return AngleC;
+        }
+        public double GetPerimeter()
+        { 
+            Perimeter = AB + BC + CA; 
             AB = Math.Sqrt(Math.Pow((y1 - x1), 2) + Math.Pow((y2 - x2), 2));
             BC = Math.Sqrt(Math.Pow((x3 - x2), 2) + Math.Pow((y3 - y2), 2));
-            CA = Math.Sqrt(Math.Pow((x3 - x1), 2) + Math.Pow((y3 - y1), 2));
-            AngleA = Math.Acos((AB * AB + CA * CA - BC * BC) / (2 * AB * CA)) * 180 / Math.PI;
-            AngleB = Math.Acos((AB * AB + BC * BC - CA * CA) / (2 * AB * BC)) * 180 / Math.PI;
-            AngleC = Math.Acos((BC * BC + CA * CA - AB * AB) / (2 * BC * CA)) * 180 / Math.PI;
-            Perimeter = AB + BC + CA;
-            p = Perimeter/2;
-            Square = Math.Sqrt(p*(p-AB)*(p-BC)*(p-CA));
-            Print();
+            CA = Math.Sqrt(Math.Pow((x3 - x1), 2) + Math.Pow((y3 - y1), 2)); 
+            return Perimeter;
         }
-
-        private void Print()
+        public double GetSquare() 
         {
-            Console.WriteLine ("Сторона AB равна:" + AB);
-            Console.WriteLine ("Сторона BА равна:" + BC);
-            Console.WriteLine ("Сторона СА равна:" + CA);
-            Console.WriteLine ("Угол А равен:" + AngleA);
-            Console.WriteLine ("Угол B равен:" + AngleB);
-            Console.WriteLine ("Угол C равен:" + AngleC);
-            Console.WriteLine ("Периметр равен:" + Perimeter);
-            Console.WriteLine ("Полупериметр равен:" + p);
-            Console.WriteLine ("Площадь равна:" + Square);
+            AB = Math.Sqrt(Math.Pow((y1 - x1), 2) + Math.Pow((y2 - x2), 2));
+            BC = Math.Sqrt(Math.Pow((x3 - x2), 2) + Math.Pow((y3 - y2), 2)); 
+            CA = Math.Sqrt(Math.Pow((x3 - x1), 2) + Math.Pow((y3 - y1), 2)); 
+            p = Perimeter / 2; 
+            Square = Math.Sqrt(p*(p-AB)*(p-BC)*(p-CA)); 
+            return Square;
         }
-
     } 
-  
 }
