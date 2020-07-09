0ae6b5e1c321fdc19645217a06afe1bc447f7c33
ashoshosho <svdovnar3@gmail.com>
2020-07-09 06:32:04 +0300

    xz

---
+++ TrinagleHomeWork/Figure.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TrinagleHomeWork
+{
+    public class Figure()
+    {
+        
+    }
+}
---
+++ TrinagleHomeWork/SqareLogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TrinagleHomeWork
+{
+    public class SqareLogger
+    {
+        public void OutPutSqare(Figure figure)
+        {
+            Console.WriteLine($"Площадь круга: {circle.GetSquare()}");
+        }
+    }
+}
